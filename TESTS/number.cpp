abdb8a289057a3cff6c7b03436da80ad8ac24b6d
soudael <soudaelyn@gmail.com>
2025-02-15 14:27:31 -0300

    Number Generation

--- TESTS/number.cpp
+++ TESTS/number.cpp
@@ -1,57 +1,103 @@
 #include <iostream>
 #include <ctime>
 
-bool testPosition(int matrix[9][9], int position, int number);
+bool testPosition(int matrix[9][9], int position, int index);
+bool testSquare(int (&square)[3][3][9], int index, int position, int i);
 
 int main()
 {
     srand(time(NULL));
 
     int matrix[9][9] = {0};
+    int square[3][3][9] = {0};
 
-    for(int a=1; a<10; a++){
-        int pos;
+    for(int index=1; index<10; index++){
+        int position, tempValue;
         for(int i=0; i<9; i++){
-            pos = rand() % 9;
-            while(matrix[i][pos] != a && matrix[i][pos] != 0){
-                if(pos == 8){
-                    pos = 0;
-                } else
-                if(pos >= 0){
-                    pos += 1;
-                }
-            }
-            matrix[i][pos] = a;
+            position = rand() % 9;
+            tempValue = matrix[i][position];
+            matrix[i][position] = index;
+            int attempts = 0; 
 
-            while(!testPosition(matrix, pos, a)){
-                matrix[i][pos] = 0;
-                pos = (pos + 1 + rand() % (9 - pos)) % 9;
-                while(matrix[i][pos] != 0 && !testPosition(matrix, pos, a)){
-                    pos = (pos + 1) % 9;
+            while(tempValue != 0 || !testPosition(matrix, position, index) || !testSquare(square, index, position, i)){
+                matrix[i][position] = tempValue;
+                position = (1 + position) % 9;
+                tempValue = matrix[i][position];
+                matrix[i][position] = index;
+
+                attempts++;
+                std::cout << "Tentando gerar...\n";
+                if(attempts > 18){
+                    matrix[i][position] = tempValue;
+                    break;
                 }
-                matrix[i][pos] = a;
             }
+            if(attempts > 18){
+                std::cout << "\nNao conseguiu gerar o valor " << index << ".\n";
+                std::cout << "Na linha: " << i+1 << ".\n\n";
+                continue;
+            }
+            matrix[i][position] = index;
+            std::cout << "Gerou com sucesso!\n\n";
         }
+        std::cout << "Completou " << index << " com sucesso!\n\n";
+
+        for(int j=0; j<9; j++){
+            for(int k=0; k<9; k++){
+                std::cout << matrix[j][k] << ' ';
+            }
+            std::cout << '\n'; 
+        }
+        std::cout << '\n';    
     }
 
     for(int i=0; i<9; i++){
         for(int j=0; j<9; j++){
             std::cout << matrix[i][j] << ' ';
         }
-        std::cout << '\n';
+        std::cout << '\n'; 
     }
 
     return 0;
 }
 
-bool testPosition(int matrix[9][9], int position, int number)
+bool testPosition(int matrix[9][9], int position, int index)
 {
     int counter = 0;
-
     for(int i=0; i<9; i++){
-        if(matrix[i][position] == number){
+        if(matrix[i][position] == index)
+        counter++;
+
+        if(counter > 1)
+        return false;
+    }
+    return true;
+}
+
+bool testSquare(int (&square)[3][3][9], int index, int position, int i)
+{
+    int tempMatrix[3][3], number = 0;
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            number++;
+            tempMatrix[i][j] = number;
+        }
+    }
+    int boxRow = i/3, boxCol = position/3;
+    int box = tempMatrix[boxRow][boxCol];
+
+    int row = i % 3;
+    int col = position % 3;
+    square[row][col][box] = index;
+
+    int counter = 0;
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            if(square[i][j][box] == index)
             counter++;
-            if(counter > 1) return false;
+
+            if(counter > 1)
+            return false;
         }
     }
     return true;
